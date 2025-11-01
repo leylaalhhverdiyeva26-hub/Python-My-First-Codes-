efff446caabe7c27dca95e79884b44b94fdf6208
leylaalhhverdiyeva26-hub <leylaalhhverdiyeva26@gmail.com>
2025-11-01 23:39:17 +0400

    Update and rename Merhaba Dunya to 03_String_Indeksleme.py

---
+++ 03_String_Indeksleme.py
@@ -0,0 +1,14 @@
+mesaj = "Merhaba"
+mesaj2 = "Dunya"
+mesaj3 ="bu qeder"
+print(mesaj+" "+mesaj2)
+print (mesaj[0:4])
+print (mesaj[-2])
+print (mesaj[: :-1])
+print (mesaj.upper())
+print (mesaj.lower())
+print (mesaj3.capitalize())
+print (mesaj.startswith("Me")
+print(mesaj.endswith("a"))
+print(len(mesaj+mesaj2+mesaj3))
+print("Merhaba"*10)
