2f6bedcdf70ebf98de4d0ccbd47d33a04c0781dd
leylaalhhverdiyeva26-hub <leylaalhhverdiyeva26@gmail.com>
2025-11-15 01:52:36 +0400

    Create İnteger.py

---
+++ İnteger.py
@@ -0,0 +1,42 @@
+number1=5
+number2=9.5
+print (type(number1))
+print (type(number2))
+print(5**100)
+print(14/6)
+print(14//6)
+print(abs(-2.17))
+print(round(number2))
+number=5.3957295
+print(round(number))
+print(round(number,3)
+print((3+2)*6+4)
+
+COMPARİSON OPERATORS:
+print(5==5)
+number3=8
+number4=10
+number5=10
+print(number3<number4)
+print(number3 != number4)
+print(number3 <= number5)
+a=8
+b=a
+a=9
+print(b)
+sayı1="100"
+sayı2=100
+sayı3=int(sayı1)
+print(sayı2 == sayı3)
+sayı3=int(3.9)
+print(sayı3)
+eded=234
+eded2=str(eded)
+print(type(eded2))
+print(type(eded))
+l=50
+l/=6
+print(l)
+l=30
+l*=5
+print(l)
